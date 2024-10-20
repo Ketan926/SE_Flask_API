9932549b787e721beaddd55868dbb2cca65687f5
Ketan Goud <147321329+Ketan926@users.noreply.github.com>
2024-10-20 20:44:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -101,5 +101,5 @@ import os
 
 # Run the Flask app
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Get the port from the environment
+    port = int(os.environ.get('PORT', 10000))  # Get the port from the environment
     app.run(host='0.0.0.0', port=port, debug=True)  # Bind to 0.0.0.0
